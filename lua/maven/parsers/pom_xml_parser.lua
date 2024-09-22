6d0a52e58c31e565ada7b81450d99e6dc7e48bba
oclay <ocleidyreve@gmail.com>
2024-09-22 11:28:57 -0400

    feat: implement pom xml parser using xml2lua lib

--- lua/maven/parsers/pom_xml_parser.lua
+++ lua/maven/parsers/pom_xml_parser.lua
@@ -1,45 +1,67 @@
-local Project = require('maven.sources.project')
+local xml2lua = require('xml2lua')
+local handler = require('xmlhandler.tree')
 
 ---@class PomParser
----@field private xml string
+---@field pom_xml_path string
+---@field private _xml table
 local PomParser = {}
 PomParser.__index = PomParser
 
 ---@param pom_xml_path string
 ---@return PomParser
-function PomParser.parse(pom_xml_path)
-  local file = assert(io.open(pom_xml_path, 'r'))
-  local file_content = file:read('*a')
-  file:close()
+function PomParser.new(pom_xml_path)
   local self = {}
   setmetatable(self, PomParser)
-  self.xml = file_content
+  self.pom_xml_path = pom_xml_path
   return self
+end
+
+---Parse the xml file
+function PomParser:parse()
+  local content = xml2lua.loadFile(self.pom_xml_path)
+  local xml_handler = handler:new()
+  local xml_parser = xml2lua.parser(xml_handler)
+  xml_parser:parse(content)
+  self._xml = xml_handler.root
+  assert(self._xml.project, 'Tag <project> not found on pom file')
 end
 
 ---@return string
 function PomParser:get_group_id()
-  return 'org.example.oclay1st'
+  return self._xml.project.groupId
 end
 
 ---@return string
 function PomParser:get_artifact_id()
-  return 'maven'
+  return assert(
+    self._xml.project.artifactId,
+    'Tag <artifactId> not found on pom file: ' .. self.pom_xml_path
+  )
 end
 
 ---@return string
 function PomParser:get_version()
-  return '1.0.0'
+  return self._xml.project.version
 end
 
 ---@return string
 function PomParser:get_name()
-  return 'Maven'
+  return self._xml.project.name
 end
 
 ---@return string[]
-function PomParser:get_modules()
-  return {}
+function PomParser:get_modules_paths()
+  local modules = {}
+  if self._xml.project.modules then
+    if vim.islist(self._xml.project.modules.module) then
+      for _, item in ipairs(self._xml.project.modules.module) do
+        table.insert(modules, item)
+      end
+    else
+      table.insert(modules, self._xml.project.modules.module)
+    end
+  end
+  return modules
 end
 
 return PomParser
