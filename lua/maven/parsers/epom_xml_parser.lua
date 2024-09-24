21d4c66babc645b490460f02976bd44245df8ab4
oclay <ocleidyreve@gmail.com>
2024-09-24 18:54:16 -0400

    refactor: change the epom xml parser

--- lua/maven/parsers/epom_xml_parser.lua
+++ lua/maven/parsers/epom_xml_parser.lua
@@ -1,76 +1,64 @@
-local Project = require('maven.sources.project')
 local xml2lua = require('xml2lua')
 local handler = require('xmlhandler.tree')
 
+---@class EffectivePomPlugin
+---@field group_id string
+---@field artifact_id string
+---@field version string
+
+---@class EffectivePom
+---@field group_id string
+---@field artifact_id string
+---@field version string
+---@field plugins EffectivePomPlugin[]
+
 ---@class EffectivePomParser
----@field  private _xml  any
----@field  epom_xml_path  string
 local EffectivePomParser = {}
-EffectivePomParser.__index = EffectivePomParser
 
----@param epom_xml_path string
----@return EffectivePomParser
-function EffectivePomParser.new(epom_xml_path)
-  local self = {}
-  setmetatable(self, EffectivePomParser)
-  self.epom_xml_path = epom_xml_path
-  return self
-end
-
----Parse the epom file
-function EffectivePomParser:parse()
-  local content = xml2lua.loadFile(self.epom_xml_path)
-  local xml_handler = handler:new()
-  local xml_parser = xml2lua.parser(xml_handler)
-  xml_parser:parse(content)
-  self._xml = xml_handler.root
-end
-
----@return string
-function EffectivePomParser:get_group_id()
-  return self._xml.project.groupId
-end
-
----@return string
-function EffectivePomParser:get_artifact_id()
-  return assert(
-    self._xml.project.artifactId,
-    'Tag <artifactId> not found on epom file: ' .. self.epom_xml_path
-  )
-end
-
----@return string
-function EffectivePomParser:get_version()
-  return self._xml.project.version
-end
-
----@return string
-function EffectivePomParser:get_name()
-  return self._xml.project.name
-end
-
----@return Project.Plugin[]
-function EffectivePomParser:get_plugins()
-  assert(self._xml.project.build, 'Tag <build> not found on epom file: ' .. self.epom_xml_path)
-  assert(
-    self._xml.project.build.plugins,
-    'Tag <plugins> not found on epom file: ' .. self.epom_xml_path
-  )
-  local plugins = {} ---@type table
-  local data = self._xml.project.build.plugins.plugin
+---@return EffectivePomPlugin[]
+local function build_epom_plugins(_xml)
+  assert(_xml.project.build, 'Tag <build> not found on epom')
+  assert(_xml.project.build.plugins, 'Tag <plugins> not found on epom')
+  local plugins = {} ---@type EffectivePomPlugin[]
+  local data = _xml.project.build.plugins.plugin
   if vim.islist(data) then
     for _, item in ipairs(data) do
-      local group_id = item.groupId or 'org.apache.maven.plugins'
-      local artifact_id = item.artifactId
-      local version = item.version
-      local plugin = Project.Plugin(group_id, artifact_id, version)
-      table.insert(plugins, plugin)
+      table.insert(plugins, {
+        group_id = item.groupId or 'org.apache.maven.plugins',
+        artifact_id = item.artifactId,
+        version = item.version,
+      })
     end
   else
-    local plugin = Project.Plugin(data.groupId, data.artifactId, data.version)
-    table.insert(plugins, plugin)
+    table.insert(plugins, {
+      group_id = data.groupId or 'org.apache.maven.plugins',
+      artifact_id = data.artifactId,
+      version = data.version,
+    })
   end
   return plugins
+end
+
+---Parse the epom xml content
+---@return EffectivePom
+function EffectivePomParser.parse(epom_xml_content)
+  local xml_handler = handler:new()
+  local xml_parser = xml2lua.parser(xml_handler)
+  xml_parser:parse(epom_xml_content)
+  _xml = xml_handler.root
+  assert(_xml.project, 'Tag <plugin> not found on epom file')
+  return {
+    group_id = assert(_xml.project.groupId, 'Tag <groupId> not found on epom file'),
+    artifact_id = assert(_xml.project.artifactId, 'Tag <artifactId> not found on epom file'),
+    version = assert(_xml.project.version, 'Tag <version> not found on epom file'),
+    plugins = build_epom_plugins(_xml),
+  }
+end
+
+---Parse the epom xml file
+function EffectivePomParser.parse_file(epom_xml_path)
+  local content = xml2lua.loadFile(epom_xml_path)
+  return EffectivePomParser.parse(content)
 end
 
 return EffectivePomParser
