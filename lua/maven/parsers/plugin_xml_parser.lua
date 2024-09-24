42c7835d924471c5460c1643ef3f062bff6a5182
oclay <ocleidyreve@gmail.com>
2024-09-24 11:51:32 -0400

    refactor: change the plugin parser class

--- lua/maven/parsers/plugin_xml_parser.lua
+++ lua/maven/parsers/plugin_xml_parser.lua
@@ -2,42 +2,13 @@ local Project = require('maven.sources.project')
 local xml2lua = require('xml2lua')
 local handler = require('xmlhandler.tree')
 
----@class PluginParser
----@field  private _xml  any
----@field  plugin_xml_path  string
 local PluginParser = {}
-PluginParser.__index = PluginParser
-
----Create a new Plugin xml parser
----@param plugin_xml_path string
----@return PluginParser
-function PluginParser.new(plugin_xml_path)
-  local self = {}
-  setmetatable(self, PluginParser)
-  self.plugin_xml_path = plugin_xml_path
-  return self
-end
-
----Parse the plugin xml file
-function PluginParser:parse()
-  local content = xml2lua.loadFile(self.plugin_xml_path)
-  local xml_handler = handler:new()
-  local xml_parser = xml2lua.parser(xml_handler)
-  xml_parser:parse(content)
-  self._xml = xml_handler.root
-  assert(self._xml.plugin, 'Tag <plugin> not found on plugin file')
-end
-
----@return string
-function PluginParser:get_goal_prefix()
-  return assert(self._xml.plugin.goalPrefix, 'Tag <goalPrefix> not found on plugin file')
-end
 
 ---@return Project.Goal[]
-function PluginParser:get_goals()
-  assert(self._xml.plugin.mojos, 'Tag <mojos> not found on plugin file')
+local function build_goals(_xml)
+  assert(_xml.plugin.mojos, 'Tag <mojos> not found on plugin file')
   local goals = {}
-  local data = self._xml.plugin.mojos.mojo
+  local data = _xml.plugin.mojos.mojo
   if vim.islist(data) then
     for _, item in ipairs(data) do
       local goal = Project.Goal(item.goal)
@@ -48,6 +19,31 @@ function PluginParser:get_goals()
     table.insert(goals, goal)
   end
   return goals
+end
+
+---Parse the plugin xml content
+---@param xml_content string
+---@return Project.Plugin
+function PluginParser.parse(xml_content)
+  local xml_handler = handler:new()
+  local xml_parser = xml2lua.parser(xml_handler)
+  xml_parser:parse(xml_content)
+  local _xml = xml_handler.root
+  assert(_xml.plugin, 'Tag <plugin> not found on plugin file')
+  local group_id = assert(_xml.plugin.groupId, 'Tag <groupId> not found on plugin file')
+  local artifact_id = assert(_xml.plugin.artifactId, 'Tag <artifactId> not found on plugin file')
+  local version = assert(_xml.plugin.version, 'Tag <version> not found on plugin file')
+  local goal_prefix = assert(_xml.plugin.goalPrefix, 'Tag <goalPrefix> not found on plugin file')
+  local goals = build_goals(_xml)
+  return Project.Plugin(group_id, artifact_id, version, goal_prefix, goals)
+end
+
+---Parse the plugin xml file
+---@param xml_file_path string
+---@return Project.Plugin
+function PluginParser.parse_file(xml_file_path)
+  local xml_content = xml2lua.loadFile(xml_file_path)
+  return PluginParser.parse(xml_content)
 end
 
 return PluginParser
