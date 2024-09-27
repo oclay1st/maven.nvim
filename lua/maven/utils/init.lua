f656fef6c7e214f72e52e51ec6b6a96eab430994
oclay <ocleidyreve@gmail.com>
2024-09-27 19:42:03 -0400

    feat: add a job queue for the console

--- lua/maven/utils/init.lua
+++ lua/maven/utils/init.lua
@@ -2,6 +2,11 @@ local random = math.random
 local Path = require('plenary.path')
 local M = {}
 
+M.STARTED_STATE = 'STARTED'
+M.SUCCEED_STATE = 'SUCCEED'
+M.FAILED_STATE = 'FAILED'
+M.PENDING_STATE = 'PENDING'
+
 M.uuid = function()
   local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
   return string.gsub(template, '[xy]', function(c)
@@ -12,7 +17,7 @@ end
 
 M.maven_data_path = Path:new(vim.fn.stdpath('cache'), 'maven'):absolute()
 
-M.maven_local_repository_path = Path:new(vim.loop.os_homedir(), '.m2', 'repository'):absolute()
+M.maven_local_repository_path = Path:new(Path.path.home, '.m2', 'repository'):absolute()
 
 M.maven_plugin_xml_path = 'META-INF/maven/plugin.xml'
 
