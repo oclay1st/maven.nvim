38223abbf0efa06c744e61352c13a64651824614
oclay <ocleidyreve@gmail.com>
2024-09-26 22:15:24 -0400

    refactor: update the config options

--- lua/maven/config.lua
+++ lua/maven/config.lua
@@ -3,7 +3,7 @@ local M = {}
 
 M.namespace = vim.api.nvim_create_namespace('maven')
 
----@class ExplorerWindow
+---@class ExplorerView
 ---@field position string
 ---@field size integer
 
@@ -12,14 +12,29 @@ M.namespace = vim.api.nvim_create_namespace('maven')
 ---@field description string
 ---@field cmd_args string[] the list of args
 
+---@class ConsoleView
+---@field show_lifecycle_execution boolean
+---@field show_command_execution boolean
+---@field show_plugin_goal_execution boolean
+---@field show_dependencies_load_execution boolean
+---@field show_plugins_load_execution boolean
+
 ---@class MavenOptions
----@field explorer_window ExplorerWindow
+---@field explorer? ExplorerView
+---@field console ConsoleView
 ---@field mvn_executable string the name or path of mvn
 ---@field custom_commands CustomCommand[]
 local defaultOptions = {
-  explorer_window = {
+  explorer = {
     position = 'right',
     size = 68,
+  },
+  console = {
+    show_command_execution = true,
+    show_lifecycle_execution = true,
+    show_plugin_goal_execution = true,
+    show_dependencies_load_execution = false,
+    show_plugins_load_execution = false,
   },
   mvn_executable = 'mvn',
   custom_commands = {},
