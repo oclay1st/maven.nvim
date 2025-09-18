7a4b94b8c52775dd084658bc8415c8b9b61a428e
oclay <ocleidyreve@gmail.com>
2025-09-18 11:55:17 -0400

    chore: update the github action worflow

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 
 if ! command -v nvim &>/dev/null; then
-	echo "nvim command not found"
-	exit 1
+  echo "nvim command not found"
+  exit 1
 fi
 
-nvim -l tests/minit.lua tests
+nvim --headless --noplugin -u tests/minit.lua -c "PlenaryBustedDirectory tests/ {minimal_init = 'tests/minit.lua'}" tests
--- tests/minit.lua
+++ tests/minit.lua
@@ -1,12 +1,22 @@
-#!/usr/bin/env -S nvim -l
+vim.opt.rtp:append('.')
 
-vim.env.LAZY_STDPATH = '.tests'
-load(vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'))()
-
--- Setup lazy.nvim
-require('lazy.minit').busted({
-  spec = {
-    'MunifTanjim/nui.nvim',
-    'nvim-lua/plenary.nvim',
+local plugins = {
+  {
+    path = os.getenv('PLENARY_DIR') or '/tmp/plenary.nvim',
+    repo = 'https://github.com/nvim-lua/plenary.nvim',
   },
-})
+  {
+    path = os.getenv('NUI_DIR') or '/tmp/nui.nvim',
+    repo = 'https://github.com/MunifTanjim/nui.nvim',
+  },
+}
+
+for _, plugin in ipairs(plugins) do
+  if vim.fn.isdirectory(plugin.path) == 0 then
+    vim.fn.system({ 'git', 'clone', plugin.repo, plugin.path })
+  end
+  vim.opt.rtp:append(plugin.path)
+end
+
+vim.cmd('runtime plugin/plenary.vim')
+require('plenary.busted')
